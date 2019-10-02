498d62a5391932e517957b26d41930df79922b40
EliasGcf <eliasgabrielcf@gmail.com>
2019-10-02 18:44:21 -0300

    Criando Frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,7 +15,9 @@ mongoose.connect('mongodb+srv://eliasgcf:mongo5713@omnistack9-alqtd.mongodb.net/
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (para criação, edição)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3333,() => console.log("PORT SERVER: 3333"));
